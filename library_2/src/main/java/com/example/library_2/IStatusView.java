4d384c2959a216be7e33b9d61d2b118ce61e0fda
陈泰山 <chentaishan@noboauto.com>
2021-09-02 18:14:53 +0800

    add activity

--- library_2/src/main/java/com/example/library_2/IStatusView.java
+++ library_2/src/main/java/com/example/library_2/IStatusView.java
@@ -2,10 +2,6 @@ package com.example.library_2;
 
 public interface IStatusView {
 
-    int Success = 0;
-    int Empty = 1;
-    int Loading = 2;
-    int Error = 3;
 
     void setStatus(int status);
 
--- library_2/src/main/java/com/example/library_2/LoadingLayout.java
+++ library_2/src/main/java/com/example/library_2/LoadingLayout.java
@@ -4,25 +4,38 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoadingLayout extends FrameLayout implements IStatusView{
 
-    private final int mEmptyResId;
-    private final int mLoadingResId;
-    private final int mErrorResId;
+    public static final  int SUCCESS = 0;
+    public static final  int EMPTY = 1;
+    public static final  int LOADING = 2;
+    public static final  int ERROR = 3;
+
     LayoutInflater mInflater;
 
     int mEmptyImage;
     CharSequence mEmptyText;
-
+    private int mEmptyResId = NO_ID, mLoadingResId = NO_ID, mErrorResId = NO_ID;
+    int mContentId = NO_ID;
     int mErrorImage;
     CharSequence mErrorText, mRetryText;
+    private int mStatus;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public LoadingLayout(@NonNull Context context) {
         this(context,null);
@@ -59,14 +72,108 @@ public class LoadingLayout extends FrameLayout implements IStatusView{
     public void setStatus(int status) {
 
         switch (status){
-            case IStatusView.Success:
+            case SUCCESS:
+                setContent();
                 break;
-            case IStatusView.Error:
+            case ERROR:
+                setError();
                 break;
-            case IStatusView.Loading:
+            case LOADING:
+                setLoading();
                 break;
-            case IStatusView.Empty:
+            case EMPTY:
+                setEmpty();
                 break;
         }
     }
+
+    private void setContent() {
+
+        show(mContentId);
+    }
+
+    private void setLoading() {
+
+        show(mLoadingResId);
+    }
+
+    private void setError() {
+
+        show(mErrorResId);
+    }
+
+    private void setEmpty() {
+
+        show(mEmptyResId);
+    }
+
+    Map<Integer, View> mLayouts = new HashMap<>();
+    public void show(int layoutId) {
+        for (View view : mLayouts.values()) {
+            if (view.getVisibility() != View.GONE) {
+                view.setVisibility(GONE);
+            }
+        }
+        View layout = layout(layoutId);
+        updateStatusByLayoutId(layoutId);
+        if (layout.getVisibility() != View.VISIBLE) {
+            layout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private View layout(int layoutId) {
+        if (mLayouts.containsKey(layoutId)) {
+            return mLayouts.get(layoutId);
+        }
+        View layout = mInflater.inflate(layoutId, this, false);
+        layout.setVisibility(GONE);
+
+        addView(layout, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, Gravity.CENTER));
+
+        mLayouts.put(layoutId, layout);
+
+        if (layoutId == mEmptyResId) {
+            ImageView img = layout.findViewById(R.id.empty_image);
+            if (img != null && mEmptyImage > 0) {
+                img.setImageResource(mEmptyImage);
+            }
+            TextView view = layout.findViewById(R.id.empty_text);
+            if (view != null) {
+                view.setText(mEmptyText);
+            }
+//            if (mOnEmptyInflateListener != null) {
+//                mOnEmptyInflateListener.onInflate(layout);
+//            }
+        } else if (layoutId == mErrorResId) {
+            ImageView img = layout.findViewById(R.id.error_image);
+            if (img != null && mErrorImage > 0) {
+                img.setImageResource(mErrorImage);
+            }
+            TextView txt = layout.findViewById(R.id.error_text);
+            if (txt != null) {
+                txt.setText(mErrorText);
+            }
+            View btn = layout.findViewById(R.id.retry_button);
+//            if (btn != null) {
+//                btn.setOnClickListener(mRetryButtonClickListener);
+//            }
+//            if (mOnErrorInflateListener != null) {
+//                mOnErrorInflateListener.onInflate(layout);
+//            }
+        }
+        return layout;
+    }
+
+    private void updateStatusByLayoutId(int layoutId) {
+        if (layoutId == mLoadingResId) {
+            mStatus = LOADING;
+        } else if (layoutId == mContentId) {
+            mStatus = SUCCESS;
+        } else if (layoutId == mEmptyResId) {
+            mStatus = EMPTY;
+        } else if (layoutId == mErrorResId) {
+            mStatus = ERROR;
+        }
+    }
+
 }
